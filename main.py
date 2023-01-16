f6d69f7086a806368c47b13b719f733d0aa9a4cb
sarahhcheng <sarahcheng40@gmail.com>
2023-01-15 18:26:57 -0800

    added exceptions for value errors

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import string
 import random
 
-password_length = int(input("How many characters do you want your password to be? "))
-
-character_List = string.digits + string.ascii_letters + string.punctuation
+# First input to ask for the length of the password
+try:
+    password_length = int(input("How many characters do you want your password to be? "))
+except ValueError:
+    raise Exception('Please enter a number') from None
 
 print('What kind of characters do you want in your password? \n1.) Letters \n2.) Digits \n3.) Special characters')
 
+try:
+    option = int(input("Select one of the options by entering the corresponding number "))
+except ValueError:
+    raise Exception('Please enter a number.') from None
+
+character_List = string.digits + string.ascii_letters + string.punctuation
 password = ""
-
-int(input("Select one of the options by entering the corresponding number "))
-
-print(random.choice(character_List))
 
 for i in range(password_length):
     password += password + random.choice(character_List)

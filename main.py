1b1ffa6f218342b9cbe858288d799cf968c78b08
sarahhcheng <sarahcheng40@gmail.com>
2023-01-15 18:17:20 -0800

    added password character options input

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import string
 import random
 
-password_length = int(input("How long do you want your password to be?"))
+password_length = int(input("How many characters do you want your password to be? "))
 
 character_List = string.digits + string.ascii_letters + string.punctuation
 
+print('What kind of characters do you want in your password? \n1.) Letters \n2.) Digits \n3.) Special characters')
+
 password = ""
 
-for i in range(len(password_length)):
+int(input("Select one of the options by entering the corresponding number "))
+
+print(random.choice(character_List))
+
+for i in range(password_length):
     password += password + random.choice(character_List)
 print(password)
+
+
+
 

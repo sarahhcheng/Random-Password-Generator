988131d69185ab60052d54287f4679d0f0ef0553
sarahhcheng <sarahcheng40@gmail.com>
2023-01-15 20:27:33 -0800

    should work now

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import random
 
 # Variables:
 character_list = ""
-password = ""
 
 # First input to ask for the length of the password
 try:
@@ -21,39 +20,49 @@ except ValueError:
     raise Exception('Please enter a number.') from None
 
 # Second option selection
-try:
-    ask = input("Would you like to make another selection? Enter Y/N ")
-except ValueError:
-    raise Exception('Please enter Y/N ') from None
+if option == 4:
+    character_list += character_list + string.digits + string.ascii_letters + string.punctuation
+else:
+    try:
+        ask = input("Would you like to make another selection? Enter Y/N ")
+    except ValueError:
+        raise Exception('Please enter Y/N ') from None
 
 # Code block to determine what types of characters are going to be used in the password
-if option == 4:
-    character_List = string.digits + string.ascii_letters + string.punctuation
-elif option == 1:
-    character_list = character_list + string.ascii_letters
+if option == 1:
+    character_list += character_list + string.ascii_letters
     if ask == 'Y':
         second_option = int(input("Select one of the options by entering the corresponding number "))
         if second_option == 2:
-            character_list = character_list + string.digits
+            character_list += character_list + string.digits
         elif second_option == 3:
-            character_list = character_list + string.punctuation
+            character_list += character_list + string.punctuation
 elif option == 2:
-    character_list = character_list + string.digits
+    character_list += character_list + string.digits
     if ask == 'Y':
         second_option = int(input("Select one of the options by entering the corresponding number "))
         if second_option == 1:
-            character_list = character_list + string.ascii_letters
+            character_list += character_list + string.ascii_letters
         elif second_option == 3:
-            character_list = character_list + string.punctuation
+            character_list += character_list + string.punctuation
 elif option == 3:
-    character_list = character_list + string.punctuation
+    character_list += character_list + string.punctuation
     if ask == 'Y':
         second_option = int(input("Select one of the options by entering the corresponding number "))
         if second_option == 2:
-            character_list = character_list + string.digits
+            character_list += character_list + string.digits
         elif second_option == 1:
-            character_list = character_list + string.ascii_letters
+            character_list += character_list + string.ascii_letters
+
+password = []
 
 for i in range(password_length):
-    password += password + random.choice(character_list)
-print(password)
+    # Picking a random character from our
+    # character list
+    randomchar = random.choice(character_list)
+
+    # appending a random character to password
+    password.append(randomchar)
+
+# printing password as a string
+print("The random password is:\n" + "".join(password))
